43b59d3cea10cd80e106543a2f579f43d8e5ee40
Shubh <shubh.scb@gmail.com>
2022-02-28 19:10:49 +0530

    HAMBURGER MENU POP

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let popMenu = document.querySelector(".hamburger");
 popMenu.addEventListener("click",() =>{
-    let myHeader = document.querySelector(".main-header", "main-nav");
+    let myHeader = document.querySelector(".main-header");
     myHeader.classList.add("active");
+    let myNavbar = document.querySelector(".main-nav");
+    myNavbar.classList.add("active");
+    let myLogo = document.querySelector(".my-logo")
+    myLogo.classList.add("active");
 })
+
+/*let navText = document.querySelector("hamburger");
+navText.addEventListener("click",()=>{
+    let myTextStyle = document.querySelectorAll(".link-1", "#main-nav");
+    myTextStyle.classList.add("active");
+})*/
