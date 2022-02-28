25466401e5ff41f17b425ca3758efc005686608f
Shubh <shubh.scb@gmail.com>
2022-02-28 21:08:46 +0530

    LINT FIX

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let popMenu = document.querySelector(".hamburger");
-popMenu.addEventListener("click",() =>{
-    let myHeader = document.querySelector(".main-header");
-    myHeader.classList.add("active");
-    let myNavbar = document.querySelector(".main-nav");
-    myNavbar.classList.add("active");
-    let myLogo = document.querySelector(".my-logo")
-    myLogo.classList.add("active");
-})
+const popMenu = document.querySelector('.hamburger');
+popMenu.addEventListener('click', () => {
+  const myHeader = document.querySelector('.main-header');
+  myHeader.classList.add('active');
+  const myNavbar = document.querySelector('.main-nav');
+  myNavbar.classList.add('active');
+  const myLogo = document.querySelector('.my-logo');
+  myLogo.classList.add('active');
+});
 
-/*let navText = document.querySelector("hamburger");
+/* let navText = document.querySelector("hamburger");
 navText.addEventListener("click",()=>{
     let myTextStyle = document.querySelectorAll(".link-1", "#main-nav");
     myTextStyle.classList.add("active");
-})*/
+}) */
