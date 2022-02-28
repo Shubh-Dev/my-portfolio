bd7084a884bd63785d6586bb4174aa60382b40f3
Shubh <shubh.scb@gmail.com>
2022-02-28 15:47:20 +0530

    HAMBURGER MANIPULATE

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 let popMenu = document.querySelector(".hamburger");
-
+popMenu.addEventListener("click",() =>{
+    let myHeader = document.querySelector(".main-header");
+    myHeader.classList.add("active");
+})
