46feab43107552c7abb60991710005a125bec2c4
Shubh <shubh.scb@gmail.com>
2022-03-01 10:58:25 +0530

    ADD MOBILE MENU

--- script.js
+++ script.js
@@ -6,20 +6,70 @@ popMenu.addEventListener('click', () => {
   myNavbar.classList.add('active');
   const myLogo = document.querySelector('.my-logo');
   myLogo.classList.add('active');
-  const crossBar = document.querySelector('.enable-icon');
-  crossBar.classList.add('active');
-  const lineBar = document.querySelector('.line');
+  const lineBar = document.querySelector('.hamburger');
   lineBar.classList.add('active');
+  const imgCross = document.querySelector('.cross-img');
+  imgCross.classList.add('active');
 });
 
 const popOutMenu = document.querySelector('.cross-img');
 popOutMenu.addEventListener('click', () => {
-  const imgCross = document.querySelector('.main-header');
+  const myHeader = document.querySelector('.main-header');
+  myHeader.classList.remove('active');
+  const myNavbar = document.querySelector('.main-nav');
+  myNavbar.classList.remove('active');
+  const myLogo = document.querySelector('.my-logo');
+  myLogo.classList.remove('active');
+  const lineBar = document.querySelector('.hamburger');
+  lineBar.classList.remove('active');
+  const imgCross = document.querySelector('.cross-img');
+  imgCross.classList.remove('active');
+});
+
+const popOutMenu2 = document.querySelector('.link-3');
+popOutMenu2.addEventListener('click', () => {
+  const myHeader = document.querySelector('.main-header');
+  myHeader.classList.remove('active');
+  const myNavbar = document.querySelector('.main-nav');
+  myNavbar.classList.remove('active');
+  const myLogo = document.querySelector('.my-logo');
+  myLogo.classList.remove('active');
+  const lineBar = document.querySelector('.hamburger');
+  lineBar.classList.remove('active');
+  const imgCross = document.querySelector('.cross-img');
+  imgCross.classList.remove('active');
+});
+
+const popOutMenu3 = document.querySelector('.link-4');
+popOutMenu3.addEventListener('click', () => {
+  const myHeader = document.querySelector('.main-header');
+  myHeader.classList.remove('active');
+  const myNavbar = document.querySelector('.main-nav');
+  myNavbar.classList.remove('active');
+  const myLogo = document.querySelector('.my-logo');
+  myLogo.classList.remove('active');
+  const lineBar = document.querySelector('.hamburger');
+  lineBar.classList.remove('active');
+  const imgCross = document.querySelector('.cross-img');
+  imgCross.classList.remove('active');
+});
+
+const popOutMenu4 = document.querySelector('.link-5');
+popOutMenu4.addEventListener('click', () => {
+  const myHeader = document.querySelector('.main-header');
+  myHeader.classList.remove('active');
+  const myNavbar = document.querySelector('.main-nav');
+  myNavbar.classList.remove('active');
+  const myLogo = document.querySelector('.my-logo');
+  myLogo.classList.remove('active');
+  const lineBar = document.querySelector('.hamburger');
+  lineBar.classList.remove('active');
+  const imgCross = document.querySelector('.cross-img');
   imgCross.classList.remove('active');
 });
 
 /* let navText = document.querySelector("hamburger");
-navText.addEventListener("click",()=>{
+navText.addEventListener("click",()=> {
     let myTextStyle = document.querySelectorAll(".link-1", "#main-nav");
     myTextStyle.classList.add("active");
 }) */
