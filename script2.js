6eb3e6bc83893091ae968a31d2679892c4489bfa
Shubh <shubh.scb@gmail.com>
2022-03-05 09:23:58 +0530

    BUTTON DESIGN UPDATE

--- script2.js
+++ script2.js
@@ -4,6 +4,10 @@ const h2 = document.createElement('h2');
 h2.textContent = 'My Resume';
 h2.className = 'popUpResumeHeadline';
 div.appendChild(h2);
+const crossButton = document.createElement('img');
+crossButton.src = './images/Enabled-4.svg';
+crossButton.className = 'popResumeCrossbutton';
+div.appendChild(crossButton);
 document.getElementById('division-resumeWin').appendChild(div);
 
 const resumePopUpWin = document.querySelector('.popResumeButton');
@@ -11,7 +15,7 @@ resumePopUpWin.addEventListener('click', () => {
   const popResumeWinUP = document.querySelector('.popResume');
   popResumeWinUP.classList.add('active');
   // pop-out
-  const resumePopOutWin = document.querySelector('.popUpResumeHeadline');
+  const resumePopOutWin = document.querySelector('.popResumeCrossbutton');
   resumePopOutWin.addEventListener('click', () => {
     const popResumeWinUP = document.querySelector('.popResume');
     popResumeWinUP.classList.remove('active');
